cd064a20731a9ad65346e183279495716b123bf0
Oğuzcan Toptaş <oguzcantoptas@gmail.com>
2024-03-22 20:24:44 +0300

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,98 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+/* Challenge: 
 
-export default function Home() {
+Bu web sayfası, HTML öğeleri vanilla JS ile DOM'a eklenerek oluşturulmuştur. Göreviniz JS'yi React JSX'e dönüştürmek ve React ile sayfaya render etmek. İşte istediğimiz şeyler: 
+
+        1. Sayfa, JSX ile yazılmış diğer fonksiyonel bileşenlerden ve elementlerden oluşan tek bir fonksiyonel React bileşeni tarafından render edilmelidir.
+        
+        2. Sonuç, mevcut sayfadan ayırt edilememelidir (bonus challenge yapmadığınız sürece).
+        
+        3. Mevcut JS'nin tamamı sonunda silinmelidir. 
+        
+        4. Kod modüler ve iyi organize edilmiş olmalıdır.
+*/
+
+// const header = `<header>
+//                     <div class="button-container">
+//                         <button>Menu</button>
+//                         <button>Hakkında</button>
+//                         <button>Saatler & Lokasyon</button>
+//                     </div>
+//                 </header>`
+// document.body.innerHTML = header
+
+// const mainElement = document.createElement('main')
+// document.body.appendChild(mainElement)
+
+// const someHeading = document.createElement('h1')
+// someHeading.classList.add('logo-container')
+// someHeading.textContent = 'Basit Yemekler Restoranı'
+// mainElement.appendChild(someHeading)
+
+// const someParagraph = document.createElement('p')
+// someParagraph.classList.add('info-container')
+// someParagraph.textContent = `Basit insanlar için basit yemekler yapıyoruz. Sandviç ister misiniz? Tamamdır. Goralı? Hemen geliyor. Ateşte kavrulmuş kuşkonmaz börekleri ile yerel kaynaklı balla sırlanmış haşlanmış somon yumurtası? Hadi oradan. Caddenin karşısındaki Fantezi Yemek Kafe'yi deneyin.`
+// mainElement.appendChild(someParagraph)
+
+// const imageContainer = `<div class="image-container">
+//                             <div class="image-wrapper">
+//                                 <img class="hot-dog" src="images/hotdogs.jpg"/>
+//                             </div>
+//                             <div class="image-wrapper">
+//                                 <img src="images/sandwich.jpg"/>
+//                             </div>
+//                             <div class="image-wrapper">
+//                                 <img src="images/hamburger.jpg"/>
+//                             </div>
+//                         </div>`
+// mainElement.insertAdjacentHTML('beforeend', imageContainer)
+
+// const footer = `<footer>
+//                     <p>
+//                         Made by <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank"> Coding Company</a>™.
+//                     </p>
+//                 </footer>`
+// document.body.innerHTML += footer
+
+const Page = () => {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div>
+      <header>
+        <div class="button-container">
+          <button>Menu</button>
+          <button>Hakkında</button>
+          <button>Saatler & Lokasyon</button>
         </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+      </header>
+      <main>
+        <h1 className="logo-container">Basit Yemekler Restoranı</h1>
+        <p className="info-container">
+          Basit insanlar için basit yemekler yapıyoruz. Sandviç ister misiniz?
+          Tamamdır. Goralı? Hemen geliyor. Ateşte kavrulmuş kuşkonmaz börekleri
+          ile yerel kaynaklı balla sırlanmış haşlanmış somon yumurtası? Hadi
+          oradan. Caddenin karşısındaki Fantezi Yemek Kafe'yi deneyin.
+        </p>
+        <div class="image-container">
+          <div class="image-wrapper">
+            <img class="hot-dog" src="images/hotdogs.jpg" />
+          </div>
+          <div class="image-wrapper">
+            <img src="images/sandwich.jpg" />
+          </div>
+          <div class="image-wrapper">
+            <img src="images/hamburger.jpg" />
+          </div>
+        </div>
+      </main>
+      <footer>
+        <p>
+          Made by{' '}
+          <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" target="_blank">
+            Coding Company
+          </a>
+          ™.
+        </p>
+      </footer>
+    </div>
+  )
 }
+export default Page
